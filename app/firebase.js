4a4b0df93fd3859f419f557d9b95fb1d77b6b5d7
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-27 15:54:27 -0600

    ci: :wrench: configure firebase

---
+++ app/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpFdr8dZsYxcSyR7x8eYSnctjZq_asgy4",
+  authDomain: "expense-tracker-61b93.firebaseapp.com",
+  projectId: "expense-tracker-61b93",
+  storageBucket: "expense-tracker-61b93.appspot.com",
+  messagingSenderId: "803343091447",
+  appId: "1:803343091447:web:89eb916dee1c5aa52575f2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
